dc5c7deb5b0184d1de013f3fb146bc19bdfd6d5b
ashutilov <ashutilov@ukr.net>
2018-01-28 03:27:11 +0200

    first draft

--- src/lesson1/task.js
+++ src/lesson1/task.js
@@ -1,8 +1,9 @@
 /*
   Напишите функцию, которая принимает 1 аргумент и возварщает его тип
 */
+//TODO: -- implementation of additional checks is required!
 function getDataType(variable) {
-
+  return typeof variable;
 }
 
 /*
@@ -13,10 +14,31 @@ function getDataType(variable) {
   'object-array' - если массив
   'object-function' - если функция
 */
+//TODO: -- implementation of additional checks is required!
 function getDataTypePseudoName(variable) {
+  const dataType = typeof variable;
 
+  switch (dataType) {
+    case 'number':
+    case 'string':
+    case 'boolean':
+    case 'symbol':
+      return 'primitive';
+    case 'undefind':
+      return 'primitive-special';
+    case 'object':
+      if (variable === null) {
+        return 'primitive-special';
+      } else if (Array.isArray(variable)) {
+        return 'object-array';
+      }
+      return 'object';
+    case 'function':
+      return 'object-function';
+    default:
+      return undefined;
+  }
 }
-
 
 /*
   Напишите функцию, которая принимает 2 аргумента,
@@ -24,8 +46,15 @@ function getDataTypePseudoName(variable) {
   0 если равны только значения
   и -1 в другом случае
 */
+//TODO: -- implementation of additional checks is required!
 function compareByType(a, b) {
+  if (a === b) {
+    return 1;
+  } else if (a == b) {
+    return 0;
+  }
 
+  return -1;
 }
 
 // Numbers
@@ -36,19 +65,19 @@ function compareByType(a, b) {
   и возврвщвет результат,
   в любом другом случае возврвщвет -1
 */
+//TODO: -- implementation of additional checks is required!
 function increase(value) {
-
+  return typeof value === 'number' ? value + 1 : -1;
 }
 
 /*
   Напишите функцию, которая принимает 1 аргумент(число),
   и в случае если аргумент не Infinity или NaN возвращает строку 'safe' иначе 'danger'
 */
+//TODO: -- implementation of additional checks is required!
 function testForSafeNumber(value) {
-
+  return Number.isFinite(value) || !Number.isNaN(value) ? 'safe' : 'danger';
 }
-
-
 
 // Strings
 
@@ -56,17 +85,18 @@ function testForSafeNumber(value) {
   Напишите функцию, которая принимает 1 аргумент (строку),
   и возвращает массив из елементов строки разделенных по пробелу ' '
 */
+//TODO: -- implementation of additional checks is required!
 function stringToArray(str) {
-
+  return str.split(' ');
 }
-
 
 /*
   Напишите функцию, которая принимает 1 аргумент (строку),
   и возвращает часть этой строки до первой запятой
 */
+//TODO: -- implementation of additional checks is required!
 function getStringPart(str) {
-
+  return str.slice(0, str.indexOf(','));
 }
 
 /*
@@ -74,45 +104,72 @@ function getStringPart(str) {
   и возвращает порядковый номер симовола в строе если символ встречается в строке 1 раз,
   false в противоположном случае
 */
+//TODO: -- implementation of additional checks is required!
 function isSingleSymbolMatch(str, symbol) {
+  if (
+    str.indexOf(symbol) === str.lastIndexOf(symbol) &&
+    str.indexOf(symbol) !== -1
+  ) {
+    return str.indexOf(symbol);
+  }
 
+  return false;
 }
 
 /*
   Напишите функцию, которая принимает 2 аргумента,
   массив в разделитель[опционально],
   и возвращает строку ввиде элементов массива c разделителями если разделитель задан
-  или строку разделенную "-" если не задан
+  или строку разделенную '-' если не задан
 */
+//TODO: -- implementation of additional checks is required!
 function join(array, separator) {
-
+  return separator ? array.join(separator) : array.join('-');
 }
-
 
 /*
   Напишите функцию, которая принимает 2 массива,
   и возвращает один состоящий из элементов перового и второго (последовательно сначала первый потом второй)
 */
+//TODO: -- implementation of additional checks is required!
 function glue(arrA, arrB) {
-
+  return [...arrA, ...arrB];
 }
-
 
 /*
   Напишите функцию, которая принимает 1 массив,
   и возвращает другой массив отсортированный от большего к меньшему
 */
+//TODO: -- implementation of additional checks is required!
 function order(arr) {
+  const sortNumbers = (a, b) => b - a;
 
+  const sortLetters = (a, b) => {
+    if (a < b) {
+      return 1;
+    }
+
+    if (a > b) {
+      return -1;
+    }
+
+    return 0;
+  }
+
+  if (!arr.some(isNaN)) {
+    return arr.sort(sortNumbers);
+  }
+
+  return arr.sort(sortLetters);
 }
-
 
 /*
   Напишите функцию, которая принимает 1 массив,
   и возвращает другой без чисел которые меньше 0
 */
+//TODO: -- implementation of additional checks is required!
 function removeNegative(arr) {
-
+  return arr.filter(value => value > 0);
 }
 
 /*
@@ -121,8 +178,9 @@ function removeNegative(arr) {
   которые присутствуют во втром
   [1,2,3], [1, 3] => [2]
 */
+//TODO: -- implementation of additional checks is required!
 function without(arrA, arrB) {
-
+  return arrA.filter(el => !arrB.includes(el));
 }
 
 /*
@@ -132,8 +190,47 @@ function without(arrA, arrB) {
   Функция вычисляет выражение и возвращает число либо NaN.
   '12/6' => 2
 */
+//TODO: -- implementation of additional checks is required!
 function calcExpression(expression) {
+  const execExpression = (operandA, operandB, operation) => {
+    switch (operation) {
+      case '+':
+        return operandA + operandB;
+      case '-':
+        return operandA - operandB;
+      case '/':
+        return operandA / operandB;
+      case '*':
+        return operandA * operandB;
+      default:
+        return undefined;
+    }
+  };
 
+  const parseExpression = () => {
+    const allowedOpers = ['+', '-', '/', '*'];
+    let result;
+
+    allowedOpers.forEach((value, index) => {
+      if (expression.includes(value)) {
+        const operandA = expression.slice(0, index);
+        const operandB = expression.slice(-index);
+        result = execExpression(operandA, operandB, value);
+      }
+
+      return undefined;
+    });
+
+    if (Number.isFinite(result)) {
+      return result;
+    } else if (typeof result === 'number') {
+      return Infinity;
+    }
+
+    return NaN;
+  };
+
+  return parseExpression();
 }
 
 /*
@@ -144,9 +241,8 @@ function calcExpression(expression) {
   либо бросает exception в случае ошибки.
   '100>5' => true
 */
-function calcComparison(expression) {
-
-}
+//TODO: -- implementation required
+function calcComparison(expression) {}
 
 /*
   Напишите функцию, которая принимает обьект и строку,
@@ -156,9 +252,8 @@ function calcComparison(expression) {
   { a: { x: 2 }, b: 5 }, '.a.x' => 2
   { a: 1, b: 2 }, '.c' => exception
 */
-function evalKey(obj, expression) {
-
-}
+//TODO: -- implementation required
+function evalKey(obj, expression) {}
 
 export default {
   getDataType,
--- src/lesson2/task.js
+++ src/lesson2/task.js
@@ -1,29 +1,33 @@
 /*
   Напишите функцию, которая параметрами принимает 2 числа и возвращает их сумму
 */
+//TODO: -- implementation of additional checks AND test cases is required!
 export function sum(a, b) {
-
+  return a + b;
 }
 
 /*
   Напишите функцию, которая возвращает сумму всех чисел, что передаются параметрами
 */
-export function sumAll() {
-
+//TODO: -- implementation of additional checks AND test cases is required!
+export function sumAll(...args) {
+  return args.reduce((a, b) => a + b, 0);
 }
 
 /*
   Напишите функцию, которая возвращает число x в степень n
 */
+//TODO: -- implementation of additional checks AND test cases is required!
 export function pow(x, n) {
-
+  return x ** n;
 }
 
 /*
   Напишите функцию, которая возвращает рандомное целое число от from до to
 */
+//TODO: -- implementation of additional checks AND test cases is required!
 export function random(from, to) {
-
+  return Math.floor(Math.random() * ((to - from) + 1)) + from;
 }
 
 export default {
