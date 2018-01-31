6bf8744466f934fd4b7a4bc8ff69d25c1b00079d
ashutilov <ashutilov@ukr.net>
2018-01-31 03:09:10 +0200

    lesson1 - minor improvements

--- src/lesson1/task.js
+++ src/lesson1/task.js
@@ -1,7 +1,6 @@
 /*
   Напишите функцию, которая принимает 1 аргумент и возварщает его тип
 */
-//TODO: -- implementation of additional checks is required!
 function getDataType(variable) {
   return typeof variable;
 }
@@ -14,9 +13,12 @@ function getDataType(variable) {
   'object-array' - если массив
   'object-function' - если функция
 */
-//TODO: -- implementation of additional checks is required!
 function getDataTypePseudoName(variable) {
   const dataType = typeof variable;
+
+  if ([null, undefined].includes(variable)) {
+    return 'primitive-special';
+  }
 
   switch (dataType) {
     case 'number':
@@ -24,19 +26,15 @@ function getDataTypePseudoName(variable) {
     case 'boolean':
     case 'symbol':
       return 'primitive';
-    case 'undefind':
-      return 'primitive-special';
     case 'object':
-      if (variable === null) {
-        return 'primitive-special';
-      } else if (Array.isArray(variable)) {
+      if (Array.isArray(variable)) {
         return 'object-array';
       }
       return 'object';
     case 'function':
       return 'object-function';
     default:
-      return undefined;
+      throw new Error('Wrong data');
   }
 }
 
@@ -46,7 +44,6 @@ function getDataTypePseudoName(variable) {
   0 если равны только значения
   и -1 в другом случае
 */
-//TODO: -- implementation of additional checks is required!
 function compareByType(a, b) {
   if (a === b) {
     return 1;
@@ -65,7 +62,6 @@ function compareByType(a, b) {
   и возврвщвет результат,
   в любом другом случае возврвщвет -1
 */
-//TODO: -- implementation of additional checks is required!
 function increase(value) {
   return typeof value === 'number' ? value + 1 : -1;
 }
@@ -74,7 +70,6 @@ function increase(value) {
   Напишите функцию, которая принимает 1 аргумент(число),
   и в случае если аргумент не Infinity или NaN возвращает строку 'safe' иначе 'danger'
 */
-//TODO: -- implementation of additional checks is required!
 function testForSafeNumber(value) {
   return Number.isFinite(value) || !Number.isNaN(value) ? 'safe' : 'danger';
 }
@@ -85,7 +80,6 @@ function testForSafeNumber(value) {
   Напишите функцию, которая принимает 1 аргумент (строку),
   и возвращает массив из елементов строки разделенных по пробелу ' '
 */
-//TODO: -- implementation of additional checks is required!
 function stringToArray(str) {
   return str.split(' ');
 }
@@ -94,7 +88,6 @@ function stringToArray(str) {
   Напишите функцию, которая принимает 1 аргумент (строку),
   и возвращает часть этой строки до первой запятой
 */
-//TODO: -- implementation of additional checks is required!
 function getStringPart(str) {
   return str.slice(0, str.indexOf(','));
 }
@@ -104,7 +97,6 @@ function getStringPart(str) {
   и возвращает порядковый номер симовола в строе если символ встречается в строке 1 раз,
   false в противоположном случае
 */
-//TODO: -- implementation of additional checks is required!
 function isSingleSymbolMatch(str, symbol) {
   if (
     str.indexOf(symbol) === str.lastIndexOf(symbol) &&
@@ -122,7 +114,6 @@ function isSingleSymbolMatch(str, symbol) {
   и возвращает строку ввиде элементов массива c разделителями если разделитель задан
   или строку разделенную '-' если не задан
 */
-//TODO: -- implementation of additional checks is required!
 function join(array, separator) {
   return separator ? array.join(separator) : array.join('-');
 }
@@ -131,7 +122,6 @@ function join(array, separator) {
   Напишите функцию, которая принимает 2 массива,
   и возвращает один состоящий из элементов перового и второго (последовательно сначала первый потом второй)
 */
-//TODO: -- implementation of additional checks is required!
 function glue(arrA, arrB) {
   return [...arrA, ...arrB];
 }
@@ -140,36 +130,22 @@ function glue(arrA, arrB) {
   Напишите функцию, которая принимает 1 массив,
   и возвращает другой массив отсортированный от большего к меньшему
 */
-//TODO: -- implementation of additional checks is required!
 function order(arr) {
   const sortNumbers = (a, b) => b - a;
 
-  const sortLetters = (a, b) => {
-    if (a < b) {
-      return 1;
-    }
-
-    if (a > b) {
-      return -1;
-    }
-
-    return 0;
-  };
-
-  if (!arr.some(isNaN)) {
+  if (arr.every(x => typeof x === 'number')) {
     return arr.sort(sortNumbers);
   }
 
-  return arr.sort(sortLetters);
+  return arr.sort().reverse();
 }
 
 /*
   Напишите функцию, которая принимает 1 массив,
   и возвращает другой без чисел которые меньше 0
 */
-//TODO: -- implementation of additional checks is required!
 function removeNegative(arr) {
-  return arr.filter(value => value > 0);
+  return arr.filter(value => value >= 0);
 }
 
 /*
@@ -178,7 +154,6 @@ function removeNegative(arr) {
   которые присутствуют во втром
   [1,2,3], [1, 3] => [2]
 */
-//TODO: -- implementation of additional checks is required!
 function without(arrA, arrB) {
   return arrA.filter(el => !arrB.includes(el));
 }
@@ -190,12 +165,11 @@ function without(arrA, arrB) {
   Функция вычисляет выражение и возвращает число либо NaN.
   '12/6' => 2
 */
-//TODO: -- implementation of additional checks is required!
 function calcExpression(expression) {
-  const execExpression = (operandA, operandB, operation) => {
+  const execExpression = (operandA, operation, operandB) => {
     switch (operation) {
       case '+':
-        return operandA + operandB;
+        return Number(operandA) + Number(operandB);
       case '-':
         return operandA - operandB;
       case '/':
@@ -203,34 +177,13 @@ function calcExpression(expression) {
       case '*':
         return operandA * operandB;
       default:
-        return undefined;
+        throw new Error('Wrong expression');
     }
   };
 
-  const parseExpression = () => {
-    const allowedOpers = ['+', '-', '/', '*'];
-    let result;
-
-    allowedOpers.forEach((value, index) => {
-      if (expression.includes(value)) {
-        const operandA = expression.slice(0, index);
-        const operandB = expression.slice(-index);
-        result = execExpression(operandA, operandB, value);
-      }
-
-      return undefined;
-    });
-
-    if (Number.isFinite(result)) {
-      return result;
-    } else if (typeof result === 'number') {
-      return Infinity;
-    }
-
-    return NaN;
-  };
-
-  return parseExpression();
+  const template = /(-?\d+(\.\d+)?)\s*([+\-*/])\s*(-?\d+(\.\d+)?)/g;
+  const result = template.exec(expression);
+  return result ? execExpression(result[1], result[3], result[4]) : NaN;
 }
 
 /*
@@ -241,9 +194,12 @@ function calcExpression(expression) {
   либо бросает exception в случае ошибки.
   '100>5' => true
 */
-// FIXME: -- all test are passed with implementation based on iterators, but NEW implementation required with something less heavyweight !!!
 function calcComparison(expression) {
-  const execExpression = (operandA, operandB, operation) => {
+  const execExpression = (operandA, operation, operandB) => {
+    if (Number.isNaN(operandA) || Number.isNaN(operandB)) {
+      throw new Error('Wrong expression');
+    }
+
     switch (operation) {
       case '>':
         return operandA > operandB;
@@ -256,32 +212,18 @@ function calcComparison(expression) {
       case '<=':
         return operandA <= operandB;
       default:
-        return false;
+        throw new Error('Wrong expression');
     }
   };
 
-  const parseExpression = () => {
-    const allowedOpers = ['>=', '<=', '=', '>', '<'];
+  const template = /(-?\d+(\.\d+)?)\s*(>=|<=|=|>|<)\s*(-?\d+(\.\d+)?)/g;
+  const result = template.exec(expression);
 
-    for (const value of allowedOpers) {
-      if (expression.includes(value)) {
-        const index = expression.indexOf(value);
-        const operandA = Number(expression.slice(0, index));
-        let operandB;
-        if (value === '>=' || value === '<=') {
-          operandB = Number(expression.slice(index + 2, expression.length));
-        } else {
-          operandB = Number(expression.slice(index + 1, expression.length));
-        }
-        if (Number.isNaN(operandA) || Number.isNaN(operandB)) {
-          throw Error('Wrong params! Only numbers are required.');
-        }
-        return execExpression(operandA, operandB, value);
-      }
-    }
-  };
+  if (result) {
+    return execExpression(Number(result[1]), result[3], Number(result[4]));
+  }
 
-  return parseExpression();
+  throw new Error('Wrong expression');
 }
 
 /*
@@ -292,16 +234,18 @@ function calcComparison(expression) {
   { a: { x: 2 }, b: 5 }, '.a.x' => 2
   { a: 1, b: 2 }, '.c' => exception
 */
-//TODO: -- implementation of additional checks is required!
 function evalKey(obj, expression) {
   if (expression.startsWith('.')) {
-    const result = expression.split('.').slice(1).reduce((accumulator, index) => accumulator && accumulator[index], obj);
+    const result = expression
+      .split('.')
+      .slice(1)
+      .reduce((accumulator, index) => accumulator && accumulator[index], obj);
     if (result === undefined) {
-      throw Error('Error');
+      throw new Error('Wrong expression');
     }
     return result;
   }
-  throw Error('Error');
+  throw new Error('Wrong expression');
 }
 
 export default {
@@ -320,5 +264,5 @@ export default {
   without,
   calcExpression,
   calcComparison,
-  evalKey
+  evalKey,
 };
