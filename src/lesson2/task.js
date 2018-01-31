6650746c588fc84ed729a57417441b6111005e60
ashutilov <ashutilov@ukr.net>
2018-01-31 20:41:02 +0200

    lesson2 - minor improvements

--- src/lesson2/task.js
+++ src/lesson2/task.js
@@ -1,32 +1,64 @@
 /*
   Напишите функцию, которая параметрами принимает 2 числа и возвращает их сумму
 */
-//TODO: -- implementation of additional checks AND test cases is required!
-export function sum(a, b) {
+function sum(a, b) {
+  if (arguments.length !== 2) {
+    throw new Error('Wrong arguments number!');
+  }
+
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Wrong arguments type!');
+  }
+
   return a + b;
 }
 
 /*
   Напишите функцию, которая возвращает сумму всех чисел, что передаются параметрами
 */
-//TODO: -- implementation of additional checks AND test cases is required!
-export function sumAll(...args) {
+function sumAll(...args) {
+  if (!args.length) {
+    throw new Error('Wrong arguments number!');
+  }
+
+  if (args.some(x => typeof x !== 'number')) {
+    throw new Error('Wrong arguments type!');
+  }
+
   return args.reduce((a, b) => a + b, 0);
 }
 
 /*
   Напишите функцию, которая возвращает число x в степень n
 */
-//TODO: -- implementation of additional checks AND test cases is required!
-export function pow(x, n) {
+function pow(x, n) {
+  if (arguments.length !== 2) {
+    throw new Error('Wrong arguments number!');
+  }
+
+  if (typeof x !== 'number' || typeof n !== 'number') {
+    throw new Error('Wrong arguments type!');
+  }
+
   return x ** n;
 }
 
 /*
   Напишите функцию, которая возвращает рандомное целое число от from до to
 */
-//TODO: -- implementation of additional checks AND test cases is required!
-export function random(from, to) {
+function random(from, to) {
+  if (arguments.length !== 2) {
+    throw new Error('Wrong arguments number!');
+  }
+
+  if (typeof from !== 'number' || typeof to !== 'number') {
+    throw new Error('Wrong arguments type!');
+  }
+
+  if (from > to) {
+    throw new Error('Wrong arguments value!');
+  }
+
   return Math.floor(Math.random() * ((to - from) + 1)) + from;
 }
 
