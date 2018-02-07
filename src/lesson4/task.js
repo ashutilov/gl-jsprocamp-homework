5f09c519d0b7bec0451effd3a557f102957af0da
ashutilov <ashutilov@ukr.net>
2018-02-07 02:36:08 +0200

    lesson4 - first draft

--- src/lesson4/task.js
+++ src/lesson4/task.js
@@ -8,8 +8,53 @@
   in forEach() method.
   mySet = createSet(['a', 'b', 'c'])
 */
-export function createSet(arr) {
 
+function MySet(arr) {
+  this.values = [];
+  arr.forEach(value => this.add(value));
+}
+
+MySet.prototype = {
+  get size() {
+    return this.values.length;
+  },
+};
+
+MySet.prototype.add = function add(value) {
+  if (!this.has(value)) {
+    this.values.push(value);
+  }
+
+  return this;
+};
+
+MySet.prototype.has = function has(value) {
+  return this.values.includes(value);
+};
+
+MySet.prototype.delete = function del(value) {
+  const index = this.values.indexOf(value);
+
+  if (index !== -1) {
+    this.values.splice(index, 1);
+    return true;
+  }
+
+  return false;
+};
+
+MySet.prototype.forEach = function forEach(callback, args = this) {
+  args.values.forEach((value, index, array) => {
+    callback(value, value, array);
+  });
+};
+
+MySet.prototype.clear = function clear() {
+  this.values = [];
+};
+
+export function createSet(arr = []) {
+  return new MySet(arr);
 }
 
 /*
@@ -22,11 +67,62 @@ export function createSet(arr) {
   in forEach() method.
   myMap = createMap([['a', 1], ['b', 2], ['c', 3]])
 */
-export function createMap(arr) {
 
+function MyMap(arr) {
+  this.entries = [];
+  arr.forEach(element => this.set(...element));
+}
+
+MyMap.prototype = {
+  get size() {
+    return this.entries.length;
+  },
+};
+
+MyMap.prototype.get = function add(key) {
+  return this.entries.find(element => element[0] === key);
+};
+
+MyMap.prototype.has = function has(key) {
+  return Boolean(this.get(key));
+};
+
+MyMap.prototype.set = function add(key, value) {
+  if (!this.has(key)) {
+    this.entries.push([key, value]);
+  } else {
+    const updatedEntry = this.get(key);
+    updatedEntry[1] = value;
+  }
+
+  return this;
+};
+
+MyMap.prototype.delete = function del(key) {
+  if (this.has(key)) {
+    this.entries = this.entries.filter(element => !(element[0] === key));
+    return true;
+  }
+
+  return false;
+};
+
+MyMap.prototype.forEach = function forEach(callback, args = this) {
+  args.entries.forEach((element, index, array) => {
+    const [key, value] = element;
+    callback(value, key, array);
+  });
+};
+
+MyMap.prototype.clear = function clear() {
+  this.entries = [];
+};
+
+export function createMap(arr = []) {
+  return new MyMap(arr);
 }
 
 export default {
   createSet,
-  createMap
+  createMap,
 };
